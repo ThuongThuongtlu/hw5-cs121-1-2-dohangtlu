0c736e3db8d579ba6a370385eb05069f65a58026
Lương Thị Thương Thương <32722673+ThuongThuongtlu@users.noreply.github.com>
2018-02-26 18:14:00 +0700

    Update bai2.cpp

--- bai2.cpp
+++ bai2.cpp
@@ -8,20 +8,15 @@ int main()
 	int year;
 	char a;
 	do 
-	{
-	cout << " nhap nam " << endl;
-	cin >> year;
-	cout << year << " is " << isleapYear(year)<<endl;
-	cout<<"continue? (y/n): ";
-	cin>>a;
-	
-	}
-
-    while (a!= 'n');
+	{		cout << " nhap nam " << endl;
+		cin >> year;
+		cout << year << " is " << isleapYear(year)<<endl;
+		cout<<"continue? (y/n): ";
+		cin>>a;
+	}while (a!= 'n');
     return 0;
-    
 }
 bool isleapYear(int year)
 { 
-return ((year%4==0)&&(year%100!=0)||(year%400==0));
+	return ((year%4==0)&&(year%100!=0)||(year%400==0));
 }
